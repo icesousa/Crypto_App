8bae26bf65ae4cf4d7f33989caa7eaca45057d19
icesousa <ice-l337elite@hotmail.com>
2023-03-06 17:43:42 -0300

    novo modelo adicionado

---
+++ lib/models/historico.dart
@@ -0,0 +1,18 @@
+import 'moeda.dart';
+
+class Historico{
+  DateTime dataOperacao;
+  String tipoOperacao;
+  Moeda moeda;
+  double valor;
+  double quantidade;
+
+  Historico({
+    required this.dataOperacao,
+    required this.tipoOperacao,
+    required this.moeda,
+    required this.valor,
+    required this.quantidade,
+    
+  });
+}

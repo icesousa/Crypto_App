fd74e59f262ddf99927263b30c23073e935f2fd8
icesousa <ice-l337elite@hotmail.com>
2023-03-07 16:17:59 -0300

    comentários para melhor aprendizado

--- lib/repositories/conta_repository.dart
+++ lib/repositories/conta_repository.dart
@@ -9,10 +9,9 @@ import '../models/posicao.dart';
 
 class ContaRepository extends ChangeNotifier {
   late Database db;
-  late List<Posicao> _carteira = [];
-    late List<Historico> _historico = [];
-
-  double _saldo = 0;
+  late List<Posicao> _carteira = []; //Lista de posições das moedas na carteira do usuário
+  late List<Historico> _historico = []; //Lista de operações de compra de moedas do usuário
+  double _saldo = 0; //Saldo do usuário
 
   get saldo => _saldo;
   List<Posicao> get carteira => _carteira;
@@ -22,12 +21,14 @@ class ContaRepository extends ChangeNotifier {
     initContaRepository();
   }
 
+  //Inicializa o repositório da conta do usuário
   initContaRepository() async {
     await getSaldo();
     await _getCarteira();
     await _getHistorico();
   }
 
+  //Recupera o saldo atual da conta do usuário do banco de dados
   getSaldo() async {
     db = await DB.instance.database;
     List conta = await db.query('conta', limit: 1);
@@ -35,6 +36,7 @@ class ContaRepository extends ChangeNotifier {
     notifyListeners();
   }
 
+  //Atualiza o saldo atual da conta do usuário no banco de dados
   setSaldo(double valor) async {
     db = await DB.instance.database;
     db.update('conta', {
@@ -44,10 +46,11 @@ class ContaRepository extends ChangeNotifier {
     notifyListeners();
   }
 
+  //Executa uma operação de compra de moedas pelo usuário
   comprar(Moeda moeda, double valor) async {
     db = await DB.instance.database;
     await db.transaction((txn) async {
-      //Verificar se a moeda ja foi comprada
+      //Verificar se a moeda já foi comprada
       final posicaoMoeda = await txn.query(
         'carteira',
         where: 'sigla = ?',
@@ -90,41 +93,69 @@ class ContaRepository extends ChangeNotifier {
     notifyListeners();
   }
 
-  _getCarteira() async {
-    _carteira = [];
-    List posicoes = await db.query('carteira');
-    for (var posicao in posicoes) {
-      Moeda moeda = MoedaRepository.tabela.firstWhere(
-        (m) => m.sigla == posicao['sigla'],
-      );
-      _carteira.add(Posicao(moeda: moeda, quantidade: double.parse(posicao['quantidade'])),
-      );
-    }
-  notifyListeners();
+  //Recupera a carteira atual do usuário do banco de dados
+
+
+_getCarteira() async {
+  // Zera a lista de carteira para evitar duplicidade
+  _carteira = [];
+  
+  // Faz uma consulta no banco de dados SQLite e retorna uma lista de resultados
+  List posicoes = await db.query('carteira');
+  
+  // Itera sobre cada resultado da consulta
+  for (var posicao in posicoes) {
+    // Procura na tabela de moedas a moeda correspondente à sigla da posição atual
+    Moeda moeda = MoedaRepository.tabela.firstWhere(
+      (m) => m.sigla == posicao['sigla'],
+    );
+    
+    // Cria uma nova posição com a moeda encontrada e a quantidade da posição atual
+    _carteira.add(
+      Posicao(
+        moeda: moeda, 
+        quantidade: double.parse(posicao['quantidade']),
+      ),
+    );
   }
-
-
-
-   _getHistorico() async {
-    _historico = [];
-    List operacoes = await db.query('historico');
-    for (var operacao in operacoes) {
-      Moeda moeda = MoedaRepository.tabela.firstWhere(
-        (m) => m.sigla == operacao['sigla'],
-      );
-      _historico.add(Historico(
-        dataOperacao: DateTime.fromMillisecondsSinceEpoch(operacao['data_operacao'],),
-         tipoOperacao: operacao['tipo_operacao'], 
-         moeda: moeda,
-          valor: operacao['valor'], 
-         quantidade: double.parse(operacao['quantidade'])
-         )
-         
-      );
-      
-    }
-             
-
+  
+  // Notifica todos os ouvintes de que houve uma mudança na carteira
   notifyListeners();
+}
+
+
+
+
+_getHistorico() async {
+  // Zera a lista de histórico para evitar duplicidade
+  _historico = [];
+  
+  // Faz uma consulta no banco de dados SQLite e retorna uma lista de resultados
+  List operacoes = await db.query('historico');
+  
+  // Itera sobre cada resultado da consulta
+  for (var operacao in operacoes) {
+    // Procura na tabela de moedas a moeda correspondente à sigla da operação atual
+    Moeda moeda = MoedaRepository.tabela.firstWhere(
+      (m) => m.sigla == operacao['sigla'],
+    );
+    
+    // Cria um novo objeto Historico com as informações da operação atual
+    _historico.add(
+      Historico(
+        dataOperacao: DateTime.fromMillisecondsSinceEpoch(
+          operacao['data_operacao'],
+        ),
+        tipoOperacao: operacao['tipo_operacao'], 
+        moeda: moeda,
+        valor: operacao['valor'], 
+        quantidade: double.parse(operacao['quantidade']),
+      ),
+    );
   }
+  
+  // Notifica todos os ouvintes de que houve uma mudança no histórico
+  notifyListeners();
+}
+
 }
